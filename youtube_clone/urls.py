f2e54c153e5876326efcfeb61e803327b9a2991b
sumeet291102 <sumeet291102@gmail.com>
2024-03-04 15:33:25 +0530

    s

--- youtube_clone/urls.py
+++ youtube_clone/urls.py
@@ -38,7 +38,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
