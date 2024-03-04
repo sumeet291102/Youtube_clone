57371c5053937775886aeabc042e5a03c978beef
sumeet291102 <sumeet291102@gmail.com>
2024-03-04 18:14:55 +0530

    a

--- youtube_clone/settings.py
+++ youtube_clone/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
